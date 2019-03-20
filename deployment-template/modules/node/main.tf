697271407448c2bffbce8f26d465e82880b87e39
jonandernovella <novella.uu@gmail.com>
2019-03-20 15:01:37 +0100

    passed local addresses to rke since SNAT does not work in sunet

--- deployment-template/modules/node/main.tf
+++ deployment-template/modules/node/main.tf
@@ -31,27 +31,28 @@ resource "openstack_compute_floatingip_associate_v2" "associate_floating_ip" {
 }
 
 locals {
-  # Workaround for list not supported in conditionals (https://github.com/hashicorp/terraform/issues/12453)
-  address_list = ["${split(",", var.assign_floating_ip ? join(",", openstack_compute_floatingip_v2.floating_ip.*.address) : join(",", openstack_compute_instance_v2.instance.*.network.0.fixed_ip_v4))}"]
+ # Workaround for list not supported in conditionals (https://github.com/hashicorp/terraform/issues/12453)
+ address_list = ["${split(",", var.assign_floating_ip ? join(",", openstack_compute_floatingip_v2.floating_ip.*.address) : join(",", openstack_compute_instance_v2.instance.*.network.0.fixed_ip_v4))}"]
+ internal_address_list = ["${join(",", openstack_compute_instance_v2.instance.*.network.0.fixed_ip_v4)}"]
 }
 
 data template_file "cloud_init" {
-  template = "${file("${path.module}/${var.cloud_init_data}")}"
+ template = "${file("${path.module}/${var.cloud_init_data}")}"
 
-  vars {
-    boot_console = "centos",
-    device_path = "/dev/vda"
-  }
+ vars {
+   boot_console = "centos",
+   device_path = "/dev/vda"
+ }
 }
 
 data rke_node_parameter "node_mappings" {
-  count = "${var.count}"
+ count = "${var.count}"
 
-  address           = "${element(local.address_list, count.index)}"
-  user              = "${var.ssh_user}"
-  ssh_key_path      = "${var.ssh_key}"
-  internal_address  = "${element(openstack_compute_instance_v2.instance.*.network.0.fixed_ip_v4, count.index)}"
-  hostname_override = "${element(openstack_compute_instance_v2.instance.*.name, count.index)}"
-  role              = "${var.role}"
-  labels            = "${var.labels}"
+ address           = "${element(local.internal_address_list, count.index)}"
+ user              = "${var.ssh_user}"
+ ssh_key_path      = "${var.ssh_key}"
+ internal_address  = "${element(openstack_compute_instance_v2.instance.*.network.0.fixed_ip_v4, count.index)}"
+ hostname_override = "${element(openstack_compute_instance_v2.instance.*.name, count.index)}"
+ role              = "${var.role}"
+ labels            = "${var.labels}"
 }
